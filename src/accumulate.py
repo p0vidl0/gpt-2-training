3d672c4588df1ee4071a18b275752a7c802cd5ae
ulilana <72177110+ulilana@users.noreply.github.com>
2020-10-02 07:56:20 +0600

    Introduce new terms

--- src/accumulate.py
+++ src/accumulate.py
@@ -7,30 +7,30 @@ import time
 
 
 class AccumulatingOptimizer(object):
-    def __init__(self, opt, var_list):
-        self.opt = opt
-        self.var_list = var_list
-        self.accum_vars = {tv : tf.Variable(tf.zeros_like(tv.initialized_value()), trainable=False)
+    def __init__(own, opt, var_list):
+        own.opt = opt
+        own.var_list = var_list
+        own.accum_vars = {tv : tf.Variable(tf.zeros_like(tv.initialized_value()), trainable=False)
                            for tv in var_list}
-        self.total_loss = tf.Variable(tf.zeros(shape=[], dtype=tf.float32))
-        self.count_loss = tf.Variable(tf.zeros(shape=[], dtype=tf.float32))
+        own.total_loss = tf.Variable(tf.zeros(shape=[], dtype=tf.float32))
+        own.count_loss = tf.Variable(tf.zeros(shape=[], dtype=tf.float32))
 
-    def reset(self):
-        updates = [tv.assign(tf.zeros_like(tv)) for tv in self.accum_vars.values()]
-        updates.append(self.total_loss.assign(tf.zeros(shape=[], dtype=tf.float32)))
-        updates.append(self.count_loss.assign(tf.zeros(shape=[], dtype=tf.float32)))
+    def reset(own):
+        updates = [tv.assign(tf.zeros_like(tv)) for tv in own.accum_vars.values()]
+        updates.append(own.total_loss.assign(tf.zeros(shape=[], dtype=tf.float32)))
+        updates.append(own.count_loss.assign(tf.zeros(shape=[], dtype=tf.float32)))
         with tf.control_dependencies(updates):
             return tf.no_op()
 
-    def compute_gradients(self, loss):
-        grads = self.opt.compute_gradients(loss, self.var_list)
-        updates = [self.accum_vars[v].assign_add(g) for (g,v) in grads]
-        updates.append(self.total_loss.assign_add(loss))
-        updates.append(self.count_loss.assign_add(1.0))
+    def compute_gradients(own, loss):
+        grads = own.opt.compute_gradients(loss, own.var_list)
+        updates = [own.accum_vars[v].assign_add(g) for (g,v) in grads]
+        updates.append(own.total_loss.assign_add(loss))
+        updates.append(own.count_loss.assign_add(1.0))
         with tf.control_dependencies(updates):
             return tf.no_op()
 
-    def apply_gradients(self):
-        grads = [(g,v) for (v,g) in self.accum_vars.items()]
-        with tf.control_dependencies([self.opt.apply_gradients(grads)]):
-            return self.total_loss / self.count_loss
+    def apply_gradients(own):
+        grads = [(g,v) for (v,g) in own.accum_vars.items()]
+        with tf.control_dependencies([own.opt.apply_gradients(grads)]):
+            return own.total_loss / own.count_loss
